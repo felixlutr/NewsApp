5cb9a68b961fbc622cc91c589519fb239fb172a2
felixlutr <felixlutrovnik@gmail.com>
2024-06-26 23:17:19 +0300

    need to cleanup the .env file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,7 +5,7 @@ const NewsBoard = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=il&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setArticles(data.articles));
